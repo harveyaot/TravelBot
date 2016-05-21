9ca210275105e6e00dba0eb5ff82fc4b5eb91cdb
harvey <harveyaot@gmail.com>
2016-05-21 17:06:51 +0600

    fix restify serve static files bug

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ bot.add('/', function (session) {
 
 // Setup Restify Server
 var server = restify.createServer();
+server.get('.*', restify.serveStatic({
+  directory: __dirname,
+  default : 'index.html'
+    }));
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 server.listen(process.env.port || 9000, function () {
     console.log('%s listening to %s', server.name, server.url); 
